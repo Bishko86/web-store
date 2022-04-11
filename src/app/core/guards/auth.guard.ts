2d6a296c843d1c09a9779a5d454822a72ab3c4bf
Roman Bishko <bishko86@yahoo.com>
2022-04-11 19:40:10 +0300

    #1 | add auth and role guards

---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  isLoggedIn = false;
+  constructor(private router: Router){}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if(!this.isLoggedIn) {
+        this.router.navigate(['/login']);
+        return false
+      }
+
+    return true;
+  }
+  
+}
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,10 +2,23 @@ import { NgModule } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
-  imports: [MatDialogModule, MatMenuModule, MatIconModule, MatButtonModule],
-  exports: [MatDialogModule, MatMenuModule, MatIconModule, MatButtonModule],
+  imports: [
+    MatDialogModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSnackBarModule,
+  ],
+  exports: [
+    MatDialogModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSnackBarModule,
+  ],
 })
 export class MaterialModule {}
