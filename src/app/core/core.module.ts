039269717d44f0d2afb78a8f1894d50746bac3cd
Roman Bishko <bishko86@yahoo.com>
2022-04-14 11:39:45 +0300

    #3 | add firebase to project

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppInterceptor } from './interceptors/app.interceptor';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: AppInterceptor, multi: true}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAhr8ybWbFcR1wxIRG9VwDqQYgdrRhH0f4',
+    authDomain: 'music-web-store.firebaseapp.com',
+    projectId: 'music-web-store',
+    storageBucket: 'music-web-store.appspot.com',
+    messagingSenderId: '1085587636707',
+    appId: '1:1085587636707:web:684a32e266aafc3276e0d6',
+  },
 };
 
 /*
