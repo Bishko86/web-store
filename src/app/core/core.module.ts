5ef7b665f3f26754679c745649e0ff38208bcfd7
Roman Bishko <bishko86@yahoo.com>
2022-04-15 11:50:26 +0300

    #6 | add MaterialService

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
 import { AppInterceptor } from './interceptors/app.interceptor';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     HttpClientModule,
+    SharedModule,
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: AppInterceptor, multi: true}
