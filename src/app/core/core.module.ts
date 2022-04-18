56cce584b04991f18bc754e9673420dd88bb2e14
Roman Bishko <bishko86@yahoo.com>
2022-04-18 17:14:33 +0300

    #6 | connect ngrx store in CoreModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,6 +4,11 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppInterceptor } from './interceptors/app.interceptor';
 import { SharedModule } from '../shared/shared.module';
+import { StoreModule } from '@ngrx/store';
+import { appReducer } from './store/reducers/app.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { AppEffects } from './store/effects/app.effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [],
@@ -11,6 +16,9 @@ import { SharedModule } from '../shared/shared.module';
     CommonModule,
     HttpClientModule,
     SharedModule,
+    StoreModule.forRoot(appReducer),
+    EffectsModule.forRoot([AppEffects]),
+    StoreDevtoolsModule.instrument(),
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: AppInterceptor, multi: true}
