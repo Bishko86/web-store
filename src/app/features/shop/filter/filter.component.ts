f00b892e53afc50651103e404e53d6a514ee84cc
Roman Bishko <bishko86@yahoo.com>
2022-04-18 15:03:19 +0300

    #6 | add FilterModule to ShopModule

--- src/app/features/shop/filter/filter.component.ts
+++ src/app/features/shop/filter/filter.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   selector: 'app-filter',
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FilterComponent implements OnInit {
 
