ef4bdfaa809c367a04f611bd09ad49d16ee843a4
Roman Bishko <bishko86@yahoo.com>
2022-04-18 15:04:19 +0300

    #6 | update imports, declarations and routes in ShopModule

--- src/app/features/shop/shop-routing.module.ts
+++ src/app/features/shop/shop-routing.module.ts
@@ -1,8 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CategoryComponent } from './components/category/category.component';
-import { ProductComponent } from './components/product/product.component';
 import { ShopComponent } from './shop.component';
 
 const routes: Routes = [
@@ -10,9 +8,10 @@ const routes: Routes = [
     path: '',
     component: ShopComponent,
     children: [
-      { path: 'category/:name', component: CategoryComponent },
-      { path: 'product/:name', component: ProductComponent },
-      { path: '**', redirectTo: ''}
+      { path: '', loadChildren: () => import('./showcase/showcase.module').then((mod)=> mod.ShowcaseModule)},
+      { path: 'category/:name', loadChildren: () => import('./category/category.module').then((mod) => mod.CategoryModule) },
+      { path: 'product/:name', loadChildren: () => import('./product/product.module').then((mod)=> mod.ProductModule) },
+      { path: '**', redirectTo: '/'}
     ],
   },
 ];
--- src/app/features/shop/shop.module.ts
+++ src/app/features/shop/shop.module.ts
@@ -1,19 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProductComponent } from './components/product/product.component';
-import { CategoryComponent } from './components/category/category.component';
-import { FilterComponent } from './components/filter/filter.component';
 import { ShopComponent } from './shop.component';
 import { ShopRoutingModule } from './shop-routing.module';
+import { FilterModule } from './filter/filter.module';
 
 @NgModule({
   declarations: [
     ShopComponent,
-    ProductComponent,
-    CategoryComponent,
-    FilterComponent,
   ],
-  imports: [CommonModule, ShopRoutingModule],
-  exports: [ShopRoutingModule],
+  imports: [CommonModule, ShopRoutingModule, FilterModule],
 })
 export class ShopModule {}
