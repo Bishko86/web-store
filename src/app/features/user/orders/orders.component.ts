e6f39720c2a7d9fc7975345a63ef1b3c23fdb7a0
Roman Bishko <bishko86@yahoo.com>
2022-04-18 15:28:15 +0300

    #6 | add OrdersModule to UserModule

--- src/app/features/user/orders/orders.component.ts
+++ src/app/features/user/orders/orders.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   selector: 'app-orders',
   templateUrl: './orders.component.html',
   styleUrls: ['./orders.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class OrdersComponent implements OnInit {
 
