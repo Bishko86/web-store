41210922f1562e1012ce839ffb721c3131227fd7
Roman Bishko <bishko86@yahoo.com>
2022-04-20 14:46:12 +0300

    #6 | remove needless imports, add  provider for OrderService

--- src/app/features/user/user.module.ts
+++ src/app/features/user/user.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user.component';
-import { SharedModule } from 'src/app/shared/shared.module';
 import { UserRoutingModule } from './user-routing.module';
 import { OrderService } from './services/order.service';
 import { CartService } from './services/cart.service';
 
 @NgModule({
   declarations: [UserComponent],
-  imports: [CommonModule, UserRoutingModule, SharedModule],
+  imports: [CommonModule, UserRoutingModule],
   providers: [OrderService, CartService]
 })
 export class UserModule {}
