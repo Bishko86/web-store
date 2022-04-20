4d70d0bd5a31b93879203ad1af6b4cb9c1ac95cf
Roman Bishko <bishko86@yahoo.com>
2022-04-20 13:38:18 +0300

    #6 | restructure ShopModule

--- src/app/features/shop/showcase/showcase.module.ts
+++ src/app/features/shop/showcase/showcase.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShowcaseComponent } from './showcase.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { FilterComponent } from './components/filter/filter.component';
+import { CategoryComponent } from './components/category/category.component';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: ShowcaseComponent,
+    children: [
+      { path: 'category/:name', component: CategoryComponent },
+    ],
+  },
+];
 
 @NgModule({
-  declarations: [ShowcaseComponent],
-  imports: [
-    CommonModule,
-    RouterModule.forChild([{path: '', component: ShowcaseComponent}])
-  ],
+  declarations: [ShowcaseComponent, FilterComponent],
+  imports: [CommonModule, RouterModule.forChild(routes)],
 })
-export class ShowcaseModule { }
+export class ShowcaseModule {}
