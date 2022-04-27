1c0fa9b2f68ddfe60afd72e8fa69e5451ab3dfde
Roman Bishko <bishko86@yahoo.com>
2022-04-27 20:19:21 +0300

    #4 | dispatch isFetching action in user effects

--- src/app/core/store/effects/user.effects.ts
+++ src/app/core/store/effects/user.effects.ts
@@ -1,30 +1,37 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { Store } from '@ngrx/store';
 import { catchError, from, map, of, switchMap } from 'rxjs';
 import { User } from '../../models';
 import { AuthService } from '../../services/auth.service';
+import { isFetching } from '../actions/common.actions';
 import * as UserActionCreators from '../actions/user.actions';
+import { IAppState } from '../state/app.state';
 
 @Injectable()
 export class UserEffects {
   constructor(
     private authService: AuthService,
     private actions$: Actions,
+    private store: Store<IAppState>
   ) {}
 
   login$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(UserActionCreators.login),
       switchMap(({email, password}) => {
+        this.store.dispatch(isFetching({isFetching: true}));
         return from(this.authService.login({email, password})).pipe(
           map((data) => {
             const userDto = data?.user;
-            console.log('Login',new User(userDto));
+            this.store.dispatch(isFetching({isFetching: false}))
             return UserActionCreators.loginSuccess({
               user: new User(userDto)
             });
           }),
-          catchError((error) => of(UserActionCreators.loginFailure({ error })))
+          catchError((error) => {
+            this.store.dispatch(isFetching({isFetching: false}))
+            return of(UserActionCreators.loginFailure({ error }))})
         );
       })
     );
@@ -35,10 +42,14 @@ export class UserEffects {
       ofType(UserActionCreators.registrate),
       switchMap((action) => {
         const {username, email, password} = action;
+        this.store.dispatch(isFetching({isFetching: true}))
         return from(this.authService.registrate({username, email, password})).pipe(
           map((user) => {
+            this.store.dispatch(isFetching({isFetching: false}));
             return UserActionCreators.registrateSuccess({user: new User(user)})}),
-          catchError((error) => of(UserActionCreators.registrateFailure({ error })))
+          catchError((error) => {
+            this.store.dispatch(isFetching({isFetching: false}));
+            return of(UserActionCreators.registrateFailure({ error }))})
         );
       })
     );
