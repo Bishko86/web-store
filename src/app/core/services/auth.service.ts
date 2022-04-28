c31261089e8a09a501f1d9ea8b4791ef533ff92d
Roman Bishko <bishko86@yahoo.com>
2022-04-28 14:16:28 +0300

    #4 | sync firebase with store

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,18 +1,38 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+
+import { Store } from '@ngrx/store';
 import firebase from 'firebase/compat';
+
+import { IUser, User } from '../models';
 import { IAuthCredentials } from '../models/auth.model';
+import { loginSuccess, logoutSuccess } from '../store/actions/user.actions';
+import { selectUser } from '../store/selectors/user.selector';
+import { IAppState } from '../store/state/app.state';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthService {
-  constructor(private afAuth: AngularFireAuth) {
+export default class AuthService {
+  userIsLog: IUser | null;
+
+  constructor(
+    private afAuth: AngularFireAuth,
+    private store: Store<IAppState>
+  ) {
+    this.store.select(selectUser).subscribe((user) => (this.userIsLog = user));
+    
     this.afAuth.authState.subscribe((user) => {
       if (user) {
         localStorage.setItem('user', JSON.stringify(user));
+        if (!this.userIsLog) {
+          this.store.dispatch(loginSuccess({ user: new User(user) }));
+        }
       } else {
         localStorage.setItem('user', 'null');
+        if (this.userIsLog) {
+          this.store.dispatch(logoutSuccess());
+        }
       }
     });
   }
@@ -32,7 +52,7 @@ export class AuthService {
 
   logout(): Promise<void> {
     return this.afAuth.signOut().then(() => {
-       localStorage.removeItem('user');
-     });
-   }
+      localStorage.removeItem('user');
+    });
+  }
 }
