c354acb6416d33f542cad5d975ee1be94aadf7e9
Roman Bishko <bishko86@yahoo.com>
2022-05-05 17:39:52 +0300

    #13 | create user's cart after registration

--- src/app/core/models/cart.model.ts
+++ src/app/core/models/cart.model.ts
@@ -1,5 +1,5 @@
 export interface Cart {
-  id: string;
+  id?: string;
   clientId: string;
   products: string[];
 }
--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,30 +1,43 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection, DocumentReference } from '@angular/fire/compat/firestore';
-import { IUser } from '../models';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+  DocumentReference,
+} from '@angular/fire/compat/firestore';
+import { Cart, IUser } from '../models';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
   usersRef: AngularFirestoreCollection<IUser>;
+  cartRef: AngularFirestoreCollection<Cart>;
 
-  constructor(private firestore: AngularFirestore) { 
+  constructor(private firestore: AngularFirestore) {
     this.usersRef = this.firestore.collection('/users');
+    this.cartRef = this.firestore.collection('/cart');
   }
 
   getAll(): AngularFirestoreCollection<IUser> {
     return this.usersRef;
   }
 
-  create(user: IUser): Promise<DocumentReference<IUser>> {
-    return this.usersRef.add({...user})
+  async createUser(user: IUser): Promise<DocumentReference<IUser>> {
+    try {
+      const userCart = await this.cartRef.add({ products: [], clientId: ''});
+      const userDocument = await this.usersRef.add({ ...user, cart: userCart.id });
+      await this.cartRef.doc(userCart.id).update({ clientId: userDocument.id });
+      return userDocument;
+    } catch (err: any) {
+      return err;
+    }
   }
 
-  update(id: string, data: any): Promise<void> {
+  updateUser(id: string, data: any): Promise<void> {
     return this.usersRef.doc(id).update(data);
   }
 
-  delete(id: string): Promise<void> {
+  deleteUser(id: string): Promise<void> {
     return this.usersRef.doc(id).delete();
   }
 }
