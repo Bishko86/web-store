a4bcc6b5539f937b822fac880e47993e5d2545e0
Roman Bishko <bishko86@yahoo.com>
2022-05-17 17:16:53 +0300

    #25 | add base markup to CategoryAdminComponent

--- src/app/features/admin/category/category-admin-component.ts
+++ src/app/features/admin/category/category-admin-component.ts
@@ -1,4 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Category } from 'src/app/core/models';
+import {
+  addCategory,
+  getCategories,
+} from 'src/app/core/store/actions/category.actions';
+import {
+  selectCategories,
+  selectCategoryIsLoading,
+} from 'src/app/core/store/selectors/category.selectors';
+import { IAppState } from 'src/app/core/store/state/app.state';
+import { CategoryService } from '../services/category.service';
 
 @Component({
   selector: 'app-category-admin',
@@ -6,4 +20,41 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./category-admin-component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CategoryAdminComponent {}
+export class CategoryAdminComponent implements OnInit {
+  readonly categories$: Observable<Category[]>;
+  readonly isLoading$: Observable<boolean>;
+
+  visible = true;
+  categoryForm: FormGroup;
+
+  constructor(
+    private store: Store<IAppState>,
+    private categoryService: CategoryService
+  ) {
+    this.isLoading$ = this.store.pipe(select(selectCategoryIsLoading));
+    this.categories$ = this.store.pipe(select(selectCategories));
+  }
+
+  ngOnInit(): void {
+    this.initCategoryForm();
+    this.store.dispatch(getCategories());
+  }
+
+  private initCategoryForm() {
+    this.categoryForm = new FormGroup({
+      categoryName: new FormControl('', Validators.required),
+    });
+  }
+
+  submit() {
+    this.visible = true;
+    const { categoryName } = this.categoryForm.value;
+    this.store.dispatch(addCategory({ name: categoryName }));
+  }
+
+  remove(id: string | undefined): void {
+    if (id) {
+      this.categoryService.removeCategory(id);
+    }
+  }
+}
