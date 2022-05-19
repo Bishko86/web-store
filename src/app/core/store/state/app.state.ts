9dedcfaadc797fd916996346819458bdfeb0353b
Roman Bishko <bishko86@yahoo.com>
2022-05-19 13:25:51 +0300

    #28 | create product state

--- src/app/core/store/state/app.state.ts
+++ src/app/core/store/state/app.state.ts
@@ -1,12 +1,13 @@
-import { Order, Product } from '../../models';
+import { Order } from '../../models';
 import { initialAuthState, IAuthState } from './auth.state';
 import { ICategoryState, initialCategoryState } from './category.state';
+import { initialProductState, IProductState } from './product.state';
 import { initialUsersState, IUsersState } from './users.state';
 
 export interface IAppState {
   auth: IAuthState;
   users: IUsersState;
-  product?: Product[] | null;
+  product: IProductState;
   category: ICategoryState;
   order?: Order[];
   common: {
@@ -17,7 +18,7 @@ export interface IAppState {
 export const initialState: IAppState = {
   auth: initialAuthState,
   users: initialUsersState,
-  product: null,
+  product: initialProductState,
   category: initialCategoryState,
   common: {
     isFetching: false
