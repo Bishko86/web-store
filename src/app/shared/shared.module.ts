2aaa6f52d9ccea446d8001d313f0203acf2c8f63
Roman Bishko <bishko86@yahoo.com>
2022-10-18 21:59:35 +0300

    add ProgressComponent

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,12 +4,14 @@ import { SpinnerComponent } from './components/spinner/spinner.component';
 import { MaterialModule } from './material/material.module';
 import { ConfirmComponent } from './components/confirm/confirm.component';
 import { DragDropFileUploadDirective } from '../core/directives/drag-drop-file-upload.directive';
+import { ProgressComponent } from './components/progress/progress.component';
 
 
 const SHARED_COMPONENNTS = [
   SpinnerComponent,
   ConfirmComponent,
-  DragDropFileUploadDirective
+  DragDropFileUploadDirective,
+  ProgressComponent
 ]
 
 @NgModule({
