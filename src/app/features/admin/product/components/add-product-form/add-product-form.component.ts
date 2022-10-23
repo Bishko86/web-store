91052dc98eba7085dbfb0ea6fb9c04f40c02d391
Roman Bishko <bishko86@yahoo.com>
2022-10-23 21:15:49 +0300

    add upload image card component

--- src/app/features/admin/product/components/add-product-form/add-product-form.component.ts
+++ src/app/features/admin/product/components/add-product-form/add-product-form.component.ts
@@ -6,6 +6,7 @@ import {
   OnDestroy,
   ChangeDetectorRef,
 } from '@angular/core';
+import { AngularFireUploadTask } from '@angular/fire/compat/storage';
 import {
   AbstractControl,
   FormControl,
@@ -16,9 +17,10 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Actions, ofType } from '@ngrx/effects';
 
 import { select, Store } from '@ngrx/store';
-import { Observable, Subject, take, takeUntil } from 'rxjs';
+import { filter, Observable, Subject, take, takeUntil } from 'rxjs';
 
 import { Category, Product } from 'src/app/core/models';
+import { ConfirmService } from 'src/app/core/services/confirm.service';
 import { SnackBarService } from 'src/app/core/services/snackbar.service';
 import { UploadFileService } from 'src/app/core/services/upload-file.service';
 import {
@@ -29,10 +31,14 @@ import {
 import { selectCategories } from 'src/app/core/store/selectors/category.selectors';
 import { selectProductIsLoading } from 'src/app/core/store/selectors/product.selectors';
 import { IAppState } from 'src/app/core/store/state/app.state';
-import { TOO_MANY_FILES } from 'src/app/shared/constants/messages';
+import {
+  DELETE_CONFIRM_TEXT,
+  TOO_MANY_FILES,
+} from 'src/app/shared/constants/messages';
 import { MessageTypes } from 'src/app/shared/enums/message-types.enum';
 import { AddProductFormModel } from '../../models/add-product-form.model';
 import { ProductImage } from '../../models/product-image.model';
+import { UploadFile } from '../../models/upload-file.model';
 
 @Component({
   selector: 'app-add-product-form',
@@ -45,12 +51,14 @@ export class AddProductFormComponent implements OnInit, OnDestroy {
   public readonly accept = 'image/png, image/jpeg';
   public readonly isLoading$: Observable<boolean>;
 
+  private isEditMode: boolean = false;
+
   public categories$: Observable<Category[]>;
   public productForm: FormGroup<AddProductFormModel>;
-  public files: any[] = [];
+  public files: UploadFile[] = [];
 
-  get photoControl(): AbstractControl<ProductImage[]> | null {
-    return this.productForm.get('photo');
+  get photoControl(): AbstractControl<ProductImage[]> {
+    return this.productForm.controls['photo'];
   }
 
   constructor(
@@ -60,6 +68,7 @@ export class AddProductFormComponent implements OnInit, OnDestroy {
     private readonly fileUploadService: UploadFileService,
     private readonly cdr: ChangeDetectorRef,
     private readonly snackbarService: SnackBarService,
+    private readonly confirmService: ConfirmService,
     @Inject(MAT_DIALOG_DATA) private readonly dialogData: Product
   ) {
     this.isLoading$ = this.store.pipe(select(selectProductIsLoading));
@@ -84,29 +93,34 @@ export class AddProductFormComponent implements OnInit, OnDestroy {
     this.categories$ = this.store.pipe(select(selectCategories));
     this.updateFileList();
 
-    if(this.dialogData) {
+    if (this.dialogData) {
+      this.isEditMode = true;
       this.files = [...this.dialogData.photo];
     }
   }
 
   public saveProduct(): void {
     const product: Product = {
-      ...this.dialogData,
-      ...this.productForm.value,
-      createdAt: Date.now(),
+      ...(this.isEditMode && { id: this.dialogData.id }),
+      ...(this.productForm.value as Product),
+      createdAt: this.isEditMode ? this.dialogData.createdAt : Date.now(),
     };
-    if (this.dialogData) {
+
+    if (this.isEditMode) {
       this.store.dispatch(updateProduct({ product }));
     } else {
       this.store.dispatch(addProduct({ product }));
     }
+
     this.actions$
       .pipe(
         ofType(
           ProductActions.UPDATE_PRODUCT_SUCCESS,
-          ProductActions.ADD_PRODUCT_SUCCESS),
-          takeUntil(this.destroy$)
-        ).subscribe(() => this.dialogRef.close());
+          ProductActions.ADD_PRODUCT_SUCCESS
+        ),
+        takeUntil(this.destroy$)
+      )
+      .subscribe(() => this.dialogRef.close());
   }
 
   public onFileDropped(event: FileList): void {
@@ -114,43 +128,61 @@ export class AddProductFormComponent implements OnInit, OnDestroy {
   }
 
   public fileBrowseHandler(event: Event): void {
-    const files = (event.target as HTMLInputElement).files || {};
-    this.prepareFilesList(Object.values(files));
+    const files = Object.values((event.target as HTMLInputElement).files || {});
+    this.prepareFilesList(files);
   }
 
   private prepareFilesList(files: File[]): void {
-    if (files.length > 6) {
-      return this.snackbarService.openSnackBar(
-        TOO_MANY_FILES,
+    if (this.showWarningSnackBar(files.length + this.files.length)) {
+      return;
+    }
+
+    files.forEach((file) => {
+      const angularFireUploadTask = this.uploadFile(file);
+      const uploadFile: UploadFile = {
+        url: null,
+        name: file.name,
+        size: file.size,
+        progress$: angularFireUploadTask.percentageChanges(),
+        cancel: angularFireUploadTask.cancel,
+      };
+
+      this.files = this.files.concat(uploadFile);
+    });
+  }
+
+  private showWarningSnackBar(length: number): boolean {
+    if (length > 6) {
+      this.snackbarService.openSnackBar(
+        TOO_MANY_FILES(6),
         MessageTypes.FORBIDDEN,
         4000
       );
+      return true;
     }
-
-    for (const file of files) {
-      const progress$ = this.uploadFile(file);
-      const viewData = {
-        name: file.name,
-        size: file.size,
-        progress$,
-      };
-      this.files.push(viewData);
-    }
+    return false;
   }
 
-  private uploadFile(file: File): Observable<number | undefined> {
+  private uploadFile(file: File): AngularFireUploadTask {
     return this.fileUploadService.pushFileToStorage(file);
   }
 
   private updateFileList(): void {
     this.fileUploadService.uploadedFiles$
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((data: ProductImage[]) => {
-        if (!data.length) return;
-        const photoControl = this.productForm.get('photo');
-        const photos = [...this.dialogData.photo, ...data];
-        photoControl?.setValue(photos);
+      .pipe(filter(Boolean), takeUntil(this.destroy$))
+      .subscribe((productPhoto: ProductImage) => {
         this.cdr.markForCheck();
+        this.photoControl?.setValue([
+          productPhoto,
+          ...this.photoControl.value,
+        ]);
+
+        this.files = this.files.map((file) => {
+          if (file.name === productPhoto.name) {
+            return { ...file, url: productPhoto.url };
+          }
+          return file;
+        });
       });
   }
 
@@ -159,19 +191,41 @@ export class AddProductFormComponent implements OnInit, OnDestroy {
     this.destroy$.complete();
   }
 
-  public deleteImage(value: ProductImage): void {
+  public onDeleteProductPhoto(photoName: string): void {
     this.fileUploadService
-      .deleteFile(value.name)
+      .deleteFile(photoName)
       .pipe(take(1))
-      .subscribe(() => {
-        this.cdr.markForCheck();
-        const photos = this.photoControl
-          ?.getRawValue()
-          .filter((img: ProductImage) => img.name !== value.name);
-        this.photoControl?.setValue(photos);
-        this.files = this.files.filter(
-          (file: File) => file.name !== value.name
-        );
+      .subscribe({
+        next: () => this.deleteProductPhoto(photoName),
+        error: () => this.deleteProductPhoto(photoName),
       });
+  }
+
+  private deleteProductPhoto(photoName: string): void {
+    const productPhotos = this.photoControl
+      ?.getRawValue()
+      .filter((imag: ProductImage) => imag.name !== photoName);
+    this.photoControl?.setValue(productPhotos);
+    this.files = productPhotos;
+    this.cdr.markForCheck();
+  }
+
+  public closeDialog(): void {
+    if (this.productForm.dirty) {
+      this.confirmService
+        .confirm(DELETE_CONFIRM_TEXT)
+        .subscribe((isConfirmed) => {
+          if (isConfirmed) {
+            this.dialogRef.close();
+          }
+        });
+    } else {
+      this.dialogRef.close();
+    }
+  }
+
+  public cancelUploadImage(canceledFile: UploadFile): void {
+    canceledFile.cancel && canceledFile.cancel();
+    this.files = this.files.filter((file) => file.name !== canceledFile.name);
   }
 }
